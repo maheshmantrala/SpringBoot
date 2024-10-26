0da598bea90bf4b3c37516ef495144aa03b357ab
Mantrala Mahesh Kumar <110713786+maheshmantrala@users.noreply.github.com>
2024-10-26 11:07:22 +0530

    Add files via upload

---
+++ myspringjdbc_09Aug/src/main/java/com/mahesh/myspringjdbc_09Aug/App.java
@@ -0,0 +1,13 @@
+package com.mahesh.myspringjdbc_09Aug;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
