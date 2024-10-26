ef6d1bae813a6426c4a774d7b54287b00bb9f9c4
Mantrala Mahesh Kumar <110713786+maheshmantrala@users.noreply.github.com>
2024-10-26 11:03:45 +0530

    Add files via upload

---
+++ SpringBoot-orm/src/main/java/com/mahesh/springbootorm/SpringBootOrmApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.springbootorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOrmApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-orm/src/test/java/com/mahesh/springbootorm/SpringBootOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.springbootorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
